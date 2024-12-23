1f0bf0e33d4959ff42af39b6c6f4ca34c8c3b918
gugugabijs <skyleradamsson@gmail.com>
2024-12-23 15:21:29 +0400

    added projects

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// 1. Проверка темной темы на уровне системных настроек
+
 if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
     btnDarkMode.classList.add("dark-mode-btn--active");
 	document.body.classList.add("dark");
 }
 
-// 2. Проверка темной темы в localStorage
+
 if (localStorage.getItem('darkMode') === 'dark') {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
@@ -15,7 +15,7 @@ if (localStorage.getItem('darkMode') === 'dark') {
     document.body.classList.remove("dark");
 }
 
-// Если меняются системные настройки, меняем тему
+
 window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", (event) => {
@@ -32,7 +32,7 @@ window
 		}
     });
 
-// Включение ночного режима по кнопке
+
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
     const isDark = document.body.classList.toggle("dark");
